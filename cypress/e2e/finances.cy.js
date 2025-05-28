c5cd0d7a2aa4b284acf12120021dd00cf5508b3f
Janaina <jmtannus@gmail.com>
2025-05-28 20:33:02 -0300

    alterações beforeEach

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -2,34 +2,40 @@
 /// <reference types="cypress" />
 
 context('Dev Finances Agilizei', () => {
+
+    //hooks
+    //trechos que executam antes e depois do teste
+    //before -> antes de todos os testes
+    // beforeEach -> antes de cada teste
+    //after -> depois de todos os testes
+    // afterEach -> depois de cada teste
+
+    beforeEach(() => {
+        cy.visit('https://devfinance-agilizei.netlify.app/')
+        cy.get('#transaction .button').click() // id + classe
+
     it('Cadastrar entradas', () => {
         //- entender o fluxo manualmente
         //- mapear os elementos que vamos interagir
         //- descrever as interações com o cypress
         //- adicionar as asserções necessárias
-        cy.visit('https://devfinance-agilizei.netlify.app/')
 
         // Abre o modal de nova transação
-        cy.get('#transaction .button').click() 
+        
 
         // Preenche os campos do formulário
-        cy.get('#description').type('Mesada')
-        cy.get('[name=amount]').type('12')
-        cy.get('[type=date]').type('2025-05-28')
+        cy.get('#description').type('Presente') // id
+        cy.get('[name=amount]').type(12)
+        cy.get('[type=date]').type('2025-05-28') //atributos
 
         // Salva o formulário
-        cy.contains('button', 'Salvar').click()
+        cy.get('button').contains('Salvar').click() // tipo e valor
 
         // Aguarda a tabela ser atualizada
-        cy.get('#data-table').should('be.visible')
-
-        // Verificações
-        cy.get('#data-table tbody tr').should('have.length.at.least', 1)
-        cy.contains('#data-table tbody tr', 'Mesada').within(() => {
-            cy.get('td.description').should('have.text', 'Mesada')
-            cy.get('td.amount').should('contain', '12')
-        })
+        cy.get('#data-table tbody tr').should('have.length', 1)
+        
     });
     // Cadastrar saídas
     //Remover entradas e saídas
+    it.only('Cadastrar saídas', () => {
 }); 
