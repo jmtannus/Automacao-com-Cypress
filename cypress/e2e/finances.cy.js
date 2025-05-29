520b26e4bfefac662217f742b73bbaec044507c6
Janaina <jmtannus@gmail.com>
2025-05-28 21:09:43 -0300

    cadastrar saidas

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -13,19 +13,26 @@ context('Dev Finances Agilizei', () => {
     beforeEach(() => {
         cy.visit('https://devfinance-agilizei.netlify.app/')
         cy.get('#data-table tbody tr').should('have.length', 1)
-        cy.get('#transaction .button').click() // id + classe
 
     it('Cadastrar entradas', () => {
-        //- entender o fluxo manualmente
-        //- mapear os elementos que vamos interagir
-        //- descrever as interações com o cypress
-        //- adicionar as asserções necessárias
 
         cy.get('#transaction .button').click() // id + classe
         cy.get('#description').type('Presente') // id
-        cy.get('[name=amount]').type(12)
+        cy.get('[name=amount]').type(12) // atributos
         cy.get('[type=date]').type('2025-05-28') //atributos
         cy.get('button').contains('Salvar').click() // tipo e valor
+
+        cy.get('#data-table tbody tr').should('have.length', 1) 
+    });
+
+    it('Cadastrar saídas', () => {
+
+        cy.get('#transaction .button').click() // id + classe
+        cy.get('#description').type('Presente') // id
+        cy.get('[name=amount]').type(-12) // atributos
+        cy.get('[type=date]').type('2025-05-28') //atributos
+        cy.get('button').contains('Salvar').click() // tipo e valor
+        
         cy.get('#data-table tbody tr').should('have.length', 1) 
     });
 
